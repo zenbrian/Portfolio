5b67ed46fa45ebf2efa2d8f4eaac947d746f0d8c
JEN BOEN <zen.boen@gmail.com>
2024-10-15 00:55:08 +0800

    test2-2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: '/portfolio',
-    assetPrefix: '/portfolio/',
+    basePath: '/portfolio',  // 指定你的 GitHub Pages 路徑
+    assetPrefix: '/portfolio/',  // 為靜態資源添加正確的路徑前綴
+    images: {
+      domains: ['zenbrian.github.io'],  // 如果圖片來自外部，需要允許該域名
+      unoptimized: true,  // 禁用 Next.js 圖片優化（如果你不需要）
+    },
   };
   
   export default nextConfig;
